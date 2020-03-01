00ed6586b3892b3ee3bfda85a68f12cac55b2ec9
weslley braga <wesbragagt@gmail.com>
2020-02-29 22:22:05 -0600

    new challenge gigasecond

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,18 +1,32 @@
-export default class Raindrop{
-    convert(number: Number): String{
-        return `${number}`
-    }
-    private type(number: Number) : String {
-        if(number % 2 === 0){
-            return 'even'
-        } else {
-            return 'odd'
+export default class Raindrop {
+    convert(numberValue: number): String {
+        const factorsOfNumber = this.getFactors(numberValue)
+        let factorConvertion: any = []
+        for (let i of factorsOfNumber) {
+            let factor = factorsOfNumber[i]
+            switch (true) {
+                case numberValue === 1: factorConvertion = [1]
+                    break
+                case factor === 3: factorConvertion.push('Pling')
+                    break
+                case factor === 5: factorConvertion.push('Plang')
+                    break
+                case factor === 7: factorConvertion.push('Plong')
+                    break
+                default: factorConvertion = [factor]
+            }
         }
+        return factorConvertion.join('')
     }
-    private getFactors(number: Number) : number[]{
-        const divide = (a, b) => a * b
-        const prefill = (fn, value) => input => fn(input, value)
-        const divideBy2 = prefill(divide, 2)
-        
+
+    private getFactors(numberValue: number): number[] {
+        let factors = [1]
+        for (let i = 2; i < numberValue; i++) {
+            if (numberValue % i === 0) {
+                factors.push(i)
+            }
+        }
+        factors.push(numberValue)
+        return factors
     }
 }
